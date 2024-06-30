e10b97fcf8e953a4beddc7c44b2e95555b6aa418
Yash Sain <yashsain.2406@gmail.com>
2024-07-01 01:06:48 +0530

    minor css changes

--- src/app/DataChart/page.js
+++ src/app/DataChart/page.js
@@ -1,15 +1,17 @@
-"use client"
+"use client";
 import { useEffect, useState } from 'react';
 import { ref, onValue, off } from 'firebase/database';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { realdb } from '../../utils/firebase';
+import { UserAuth } from '../../utils/AuthContext';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export default function TransactionsChart() {
     const [transactions, setTransactions] = useState([]);
     const [loading, setLoading] = useState(true);
+    const { user } = UserAuth();
 
     useEffect(() => {
         const transactionsRef = ref(realdb, 'transactions');
@@ -35,27 +37,26 @@ export default function TransactionsChart() {
         };
     }, []);
 
-    const transactionData = transactions.reduce((acc, transaction) => {
-        const date = new Date(transaction.time).toLocaleDateString();
-        acc[date] = (acc[date] || 0) + parseFloat(transaction.amount);
+    const receivedData = transactions.reduce((acc, transaction) => {
+        if (transaction.sentTo === user.email) {
+            const date = new Date(transaction.time).toLocaleDateString();
+            acc[date] = (acc[date] || 0) + parseFloat(transaction.amount);
+        }
         return acc;
     }, {});
 
-    const labels = Object.keys(transactionData);
-    const data = Object.values(transactionData);
+    const contributedData = transactions.reduce((acc, transaction) => {
+        if (transaction.sentBy === user.email) {
+            const date = new Date(transaction.time).toLocaleDateString();
+            acc[date] = (acc[date] || 0) + parseFloat(transaction.amount);
+        }
+        return acc;
+    }, {});
 
-    const chartData = {
-        labels,
-        datasets: [
-            {
-                label: 'Total Transactions',
-                data,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                tension: 0.1,
-            },
-        ],
-    };
+    const receivedLabels = Object.keys(receivedData);
+    const receivedAmounts = Object.values(receivedData);
+    const contributedLabels = Object.keys(contributedData);
+    const contributedAmounts = Object.values(contributedData);
 
     const chartOptions = {
         scales: {
@@ -74,13 +75,48 @@ export default function TransactionsChart() {
 
     return (
         <div className='flex items-center flex-col justify-center w-[70%] mx-auto mt-10'>
-            <p className='text-3xl font-semibold mb-8'>Date wise transactions</p>
+            <p className='text-3xl font-semibold mb-8'>Date-wise Transactions</p>
             {loading ? (
                 <p className='text-lg'>Loading...</p>
             ) : (
-                <div className="w-full bg-slate-800 p-6 rounded-md">
-                <Line data={chartData} options={chartOptions} />
-                </div>
+                <>
+                    <div className="w-full bg-slate-800 p-6 rounded-md mb-8">
+                        <p className='text-2xl font-semibold mb-4'>Received</p>
+                        <Line 
+                            data={{
+                                labels: receivedLabels,
+                                datasets: [
+                                    {
+                                        label: 'Received Transactions',
+                                        data: receivedAmounts,
+                                        borderColor: 'rgba(75, 192, 192, 1)',
+                                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                        tension: 0.1,
+                                    },
+                                ],
+                            }} 
+                            options={chartOptions} 
+                        />
+                    </div>
+                    <div className="w-full bg-slate-800 p-6 rounded-md mb-10">
+                        <p className='text-2xl font-semibold mb-4'>Contributed</p>
+                        <Line 
+                            data={{
+                                labels: contributedLabels,
+                                datasets: [
+                                    {
+                                        label: 'Contributed Transactions',
+                                        data: contributedAmounts,
+                                        borderColor: 'rgba(255, 99, 132, 1)',
+                                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                        tension: 0.1,
+                                    },
+                                ],
+                            }} 
+                            options={chartOptions} 
+                        />
+                    </div>
+                </>
             )}
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import { UserAuth } from '../../utils/AuthContext';
 import { useRouter } from 'next/navigation';
-import { BsCoin } from "react-icons/bs";
+import { BsCoin, BsList } from "react-icons/bs";
 import { ref, onValue, off } from "firebase/database";
 import { realdb } from "../../utils/firebase";
 import Link from 'next/link';
@@ -12,8 +12,9 @@ export default function Header() {
     const router = useRouter();
     const {user} = UserAuth();
     const email = user?.email;
-    const [users, setUsers] = useState()
+    const [users, setUsers] = useState();
     const [selectedUser, setSelectedUser] = useState(null);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const userRef = ref(realdb, 'users');
@@ -41,7 +42,7 @@ export default function Header() {
     useEffect(() => {
         const user = users?.find(user => user.email === email);
         setSelectedUser(user);
-      }, [users]);
+    }, [users]);
 
     const handleLogout = async () => {
         try {
@@ -53,36 +54,79 @@ export default function Header() {
         }
     };
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <div className="w-full flex justify-between px-10 items-center border-b border-[#a4a3a3] py-3">
-            <Link href="/dashboard"><p className="text-xl font-semibold">Gift Manager</p></Link>
-            <div className="flex gap-4 items-center">
-            <Link href={'/recieved-contributed'}>
-                    <div className='text-lg font-semibold mr-4 hover:text-green-500'>
-                    Recieved/Contributed
+        <div className="w-full flex justify-between px-4 sm:px-10 items-center border-b border-[#a4a3a3] py-3">
+            <Link href="/dashboard">
+                <p className="text-xl font-semibold">Gift Manager</p>
+            </Link>
+            <div className="flex items-center">
+                <div className="hidden md:flex gap-4 items-center">
+                    <Link href='/recieved-contributed'>
+                        <div className='text-lg font-semibold mr-4 hover:text-green-500'>
+                            Recieved/Contributed
+                        </div>
+                    </Link>
+                    <Link href='/topUsers'>
+                        <div className='text-lg font-semibold mr-4 hover:text-green-500'>
+                            Top Users
+                        </div>
+                    </Link>
+                    <Link href='/transactions'>
+                        <div className='text-lg font-semibold mr-4 hover:text-green-500'>
+                            Transactions
+                        </div>
+                    </Link>
+                    <Link href='/DataChart'>
+                        <div className='text-lg font-semibold mr-4 hover:text-green-500'>
+                            Data Chart
+                        </div>
+                    </Link>
+                    <div className="flex gap-1 px-2 py-2 border border-[#a4a3a3] rounded-lg">
+                        <BsCoin size={24} />
+                        <span className='font-bold text-green-500'>{selectedUser?.wallet}</span>
                     </div>
-                </Link>
-                <Link href={'/topUsers'}>
-                    <div className='text-lg font-semibold mr-4 hover:text-green-500'>
-                    Top Users
-                    </div>
-                </Link>
-                <Link href={'/transactions'}>
-                    <div className='text-lg font-semibold mr-4 hover:text-green-500'>
-                    Transactions
-                    </div>
-                </Link>
-                <Link href={'/DataChart'}>
-                    <div className='text-lg font-semibold mr-4 hover:text-green-500'>
-                    Data Chart
-                    </div>
-                </Link>
-                <div className="flex gap-1 px-2 py-2 border border-[#a4a3a3] rounded-lg">
-                    <BsCoin size={24} />
-                    <span className='font-bold text-green-500'>{selectedUser?.wallet}</span>
+                    <button onClick={handleLogout} className="text-xl font-semibold">Logout</button>
                 </div>
-                <button onClick={handleLogout} className="text-xl font-semibold">Logout</button>
+                <button onClick={toggleMenu} className="md:hidden flex items-center text-xl font-semibold">
+                    <BsList size={24} />
+                </button>
             </div>
+            {menuOpen && (
+                <div className="fixed inset-0 right-0 bg-black bg-opacity-50 flex flex-col items-center justify-center transition-all duration-300 ease-in-out py-20 px-12">
+                    <div className="bg-black rounded-lg p-6">
+                        <Link href='/recieved-contributed'>
+                            <div className='text-lg font-semibold mb-4 hover:text-green-500'>
+                                Recieved/Contributed
+                            </div>
+                        </Link>
+                        <Link href='/topUsers'>
+                            <div className='text-lg font-semibold mb-4 hover:text-green-500'>
+                                Top Users
+                            </div>
+                        </Link>
+                        <Link href='/transactions'>
+                            <div className='text-lg font-semibold mb-4 hover:text-green-500'>
+                                Transactions
+                            </div>
+                        </Link>
+                        <Link href='/DataChart'>
+                            <div className='text-lg font-semibold mb-4 hover:text-green-500'>
+                                Data Chart
+                            </div>
+                        </Link>
+                        <div className="flex gap-1 px-2 py-2 border border-[#a4a3a3] rounded-lg mb-4">
+                            <BsCoin size={24} />
+                            <span className='font-bold text-green-500'>{selectedUser?.wallet}</span>
+                        </div>
+                        <button onClick={handleLogout} className="text-xl font-semibold">Logout</button> <br />
+                        <button onClick={toggleMenu} className="mt-4 text-xl font-semibold">Close</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
